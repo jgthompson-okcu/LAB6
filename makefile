c3a4fc411245d6211c0378c04652313cf937abb5
Jeffrey Thompson <jgthompson@okcu.edu>
2015-03-25 10:27:00 -0500

    first commit

--- makefile
+++ makefile
@@ -1,2 +1,3 @@
 all:
 	g++ hello.cpp -o hello
+	g++ sum.cpp -o sum
---
+++ sum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::cout;
+
+int main()
+{
+	int x;
+	int y;
+	cout << "enter x\n";
+	cin  >> x;
+	cout << "enter y\n";
+	cin >> y;
+	cout << "x+y=" << x+y;
+ 	return 0;
+}
